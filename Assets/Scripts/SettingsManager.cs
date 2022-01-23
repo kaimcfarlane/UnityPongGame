171cbda4198375bfc233fa50559392e684c58b3e
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-01-23 17:47:14 -0500

    Background Color
    Created a working settings menu that changes the background color of Pong Game. However it is only accessible from the Main Menu Screen.

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -9,19 +9,34 @@ public class SettingsManager : MonoBehaviour
 
      public string settingsMenu;
      private Color defaultColor = new Color(20f/255f, 255f/255f, 186f/255f, 0f/255f);
-     public static Color SMcolor = new Color(20f/255f, 255f/255f, 186f/255f, 0f/255f);
+     public static Color SMcolor = new Color(77f/255f, 211f/255f, 243f/255f, 0f/255f);
      public string mainMenu;
      public Camera cm;
+
 
 
     public void openSettingsMenu() {
         SceneManager.LoadScene(settingsMenu);
     }
 
-    public void changeSMcolor() { 
-        cm.backgroundColor = SMcolor;
-        SMcolor = Color.blue;
-        // SettingsManager.SMcolor = Color.blue;
+    //should change pong background and pon pause
+    public void ppColorRed() { 
+        // cm.backgroundColor = SMcolor;
+        SMcolor = Color.red;
+    }
+
+    public void ppColorStar() { 
+        // cm.backgroundColor = SMcolor;
+        SMcolor = Color.black;
+    }
+
+    public void ppColorGold() { 
+        // cm.backgroundColor = SMcolor;
+        SMcolor = new Color(250f/255f, 193f/255f, 0f/255f, 255f/255f);
+    }
+
+    public void ppColorDefault() {
+        SMcolor = defaultColor;
     }
 
     public void goMainMenu() {
